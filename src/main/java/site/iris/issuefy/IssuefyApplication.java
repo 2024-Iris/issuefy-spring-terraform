00b26d5ebdc51b010a1bb2e65c049cd6ab5393d0
lvalentine6 <ysr1227@naver.com>
2024-04-12 19:32:00 +0900

    chore: init project

---
+++ src/main/java/site/iris/issuefy/IssuefyApplication.java
@@ -0,0 +1,13 @@
+package site.iris.issuefy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuefyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuefyApplication.class, args);
+	}
+
+}
