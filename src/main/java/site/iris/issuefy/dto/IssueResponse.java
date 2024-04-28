a084d888661a458f5f921c8845f4a6535ed26db5
dokkisan <dokkisan.wang@gmail.com>
2024-04-28 23:07:05 +0900

    refactor: apply @AllArgsConstructor

--- src/main/java/site/iris/issuefy/dto/IssueResponse.java
+++ src/main/java/site/iris/issuefy/dto/IssueResponse.java
@@ -1,20 +1,15 @@
 package site.iris.issuefy.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class IssueResponse {
 	private Long id;
 	private int githubIssueNumber;
 	private String title;
 	private String label;
-
-	private IssueResponse(Long id, int githubIssueNumber, String title, String label) {
-		this.id = id;
-		this.githubIssueNumber = githubIssueNumber;
-		this.title = title;
-		this.label = label;
-	}
 
 	public static IssueResponse of(Long id, int githubIssueNumber, String title, String label) {
 		return new IssueResponse(id, githubIssueNumber, title, label);
