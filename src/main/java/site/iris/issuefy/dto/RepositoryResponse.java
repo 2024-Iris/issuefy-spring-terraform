fad2ec3b66562e60e1345519d6f8662062e58a45
dokkisan <dokkisan.wang@gmail.com>
2024-04-28 23:05:19 +0900

    refactor: apply @AllArgsConstructor

--- src/main/java/site/iris/issuefy/dto/RepositoryResponse.java
+++ src/main/java/site/iris/issuefy/dto/RepositoryResponse.java
@@ -1,19 +1,15 @@
 package site.iris.issuefy.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import site.iris.issuefy.vo.RepositoryVO;
 
 @Data
+@AllArgsConstructor
 public class RepositoryResponse {
 	private Long id;
 	private String name;
 	private String org;
-
-	private RepositoryResponse(Long id, String name, String org) {
-		this.id = id;
-		this.name = name;
-		this.org = org;
-	}
 
 	public static RepositoryResponse from(RepositoryVO repositoryVO) {
 		return new RepositoryResponse(1L, repositoryVO.name(), repositoryVO.org());
