33c7af5e65a0a48a2c99ec618ef34b8301b7b734
lvalentine6 <ysr1227@naver.com>
2024-05-04 01:05:44 +0900

    test: add OauthServiceTest

---
+++ src/test/java/site/iris/issuefy/service/OauthServiceTest.java
@@ -0,0 +1,16 @@
+package site.iris.issuefy.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OauthServiceTest {
+
+	@Test
+	void githubLogin() {
+	}
+
+	@Test
+	void getToken() {
+	}
+}
