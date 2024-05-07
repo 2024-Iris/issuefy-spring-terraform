4d7953efed9080f853532495e688a3647a86f5ab
lvalentine6 <ysr1227@naver.com>
2024-05-07 22:25:28 +0900

    fix: githubAccessTokenService value 값 주입 안됨

--- src/test/java/site/iris/issuefy/service/GithubAccessTokenServiceTest.java
+++ src/test/java/site/iris/issuefy/service/GithubAccessTokenServiceTest.java
@@ -1,32 +1,34 @@
 package site.iris.issuefy.service;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 import java.io.IOException;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 
-// 미완성 테스트 수정 예정
 @SpringBootTest
-@ActiveProfiles("application-test")
+@ActiveProfiles("test")
 class GithubAccessTokenServiceTest {
-
 	private MockWebServer mockWebServer;
 
-	private final GithubAccessTokenService githubAccessTokenService = mock(GithubAccessTokenService.class);
-
+	@DisplayName("mockWebServer로 Github API의 응답을 mocking 합니다.")
 	@BeforeEach
 	void setUp() throws IOException {
 		mockWebServer = new MockWebServer();
 		mockWebServer.start();
+		mockWebServer.enqueue(new MockResponse()
+			.setResponseCode(200)
+			.setBody("{\"access_token\": \"" + "\"testAccessToken\"" + "\", \"token_type\": \"bearer\"}")
+			.addHeader("Content-Type", "application/json"));
 	}
 
 	@AfterEach
@@ -35,19 +37,19 @@ class GithubAccessTokenServiceTest {
 	}
 
 	@Test
-	void getTokenShouldReturnAccessToken() {
+	@DisplayName("AuthenticationCode를 전송하면 AccessToken을 반환한다.")
+	void getToken() {
 		// given
+		WebClient webClient = WebClient.builder().baseUrl(mockWebServer.url("/").toString()).build();
+		GithubAccessTokenService githubAccessTokenService = new GithubAccessTokenService(webClient);
 		String authenticationCode = "testCode";
-		String expectedAccessToken = "testAccessToken";
-		mockWebServer.enqueue(new MockResponse()
-			.setResponseCode(200)
-			.setBody("{\"access_token\": \"" + expectedAccessToken + "\", \"token_type\": \"bearer\"}")
-			.addHeader("Content-Type", "application/json"));
+		String expectedResponse = "access_token=testToken&scope=&token_type=bearer";
 
-		when(githubAccessTokenService.getToken(authenticationCode)).thenReturn(
-			"access_token=testToken&scope=&token_type=bearer");
+		// when
+		String result = githubAccessTokenService.getToken(authenticationCode);
 
-		String actualAccessToken = githubAccessTokenService.getToken("dummyCode");
-
+		// then
+		assertNotNull(result);
+		assertEquals(result, expectedResponse);
 	}
 }
