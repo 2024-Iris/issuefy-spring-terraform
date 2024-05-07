3a84d2f38787789b21cf0f9e02f76274b843fcab
lvalentine6 <ysr1227@naver.com>
2024-05-08 01:05:48 +0900

    test: setter를 이용하여 필드 값 주입

--- src/test/java/site/iris/issuefy/service/GithubAccessTokenServiceTest.java
+++ src/test/java/site/iris/issuefy/service/GithubAccessTokenServiceTest.java
@@ -9,6 +9,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
@@ -23,7 +26,7 @@ class GithubAccessTokenServiceTest {
 		mockWebServer.start();
 		mockWebServer.enqueue(new MockResponse()
 			.setResponseCode(200)
-			.setBody("{\"access_token\": \"" + "\"testAccessToken\"" + "\", \"token_type\": \"bearer\"}")
+			.setBody("access_token=testToken&scope=&token_type=bearer")
 			.addHeader("Content-Type", "application/json"));
 	}
 
@@ -35,15 +38,13 @@ class GithubAccessTokenServiceTest {
 	@Test
 	@DisplayName("AuthenticationCode를 전송하면 AccessToken을 반환한다.")
 	void getToken() {
-		// given (GithubAccessTokenService 생성시 Value값이 null로 들어오는 문제로 인해 mocking 하여 진행하였습니다.)
-		// WebClient webClient = WebClient.builder().baseUrl(mockWebServer.url("/").toString()).build();
-		// GithubAccessTokenService githubAccessTokenService = new GithubAccessTokenService(webClient);
-
 		// given
+		WebClient webClient = WebClient.builder().baseUrl(mockWebServer.url("/").toString()).build();
+		GithubAccessTokenService githubAccessTokenService = new GithubAccessTokenService(webClient);
+		ReflectionTestUtils.setField(githubAccessTokenService, "clientId", "testClientId");
+		ReflectionTestUtils.setField(githubAccessTokenService, "clientSecret", "testClientSecret");
+
 		String authenticationCode = "testCode";
-		GithubAccessTokenService githubAccessTokenService = mock(GithubAccessTokenService.class);
-		when(githubAccessTokenService.getToken(authenticationCode)).thenReturn(
-			"access_token=testToken&scope=&token_type=bearer");
 		String expectedResponse = "access_token=testToken&scope=&token_type=bearer";
 
 		// when
