bccb0b63f696a47d9d9f4cb94179264460aab818
lvalentine6 <ysr1227@naver.com>
2024-05-09 01:34:29 +0900

    fix: JPA 코드 제거

--- src/main/java/site/iris/issuefy/entity/User.java
+++ src/main/java/site/iris/issuefy/entity/User.java
@@ -1,16 +1,6 @@
 package site.iris.issuefy.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-@Entity
 public class User {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
 	private Long id;
 	private String nickname;
 	private String avatarUrl;
