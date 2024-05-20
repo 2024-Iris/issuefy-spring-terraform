8ed86e7b6682f51af230a3c4f605444087c4a2f8
lvalentine6 <ysr1227@naver.com>
2024-05-20 22:21:37 +0900

    fix: 생성자 lombok 사용으로 변경

--- src/main/java/site/iris/issuefy/entity/Jwt.java
+++ src/main/java/site/iris/issuefy/entity/Jwt.java
@@ -1,16 +1,13 @@
 package site.iris.issuefy.entity;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class Jwt {
 	private final String accessToken;
 	private final String refreshToken;
-
-	private Jwt(String accessToken, String refreshToken) {
-		this.accessToken = accessToken;
-		this.refreshToken = refreshToken;
-	}
 
 	public static Jwt of(String accessToken, String refreshToken) {
 		return new Jwt(accessToken, refreshToken);
