3a711dd7955fada75b7e8fe55f344bc0fd3a9ebe
lvalentine6 <ysr1227@naver.com>
2024-05-21 04:03:23 +0900

    fix: entity 생성자 수정

--- src/main/java/site/iris/issuefy/entity/Issue.java
+++ src/main/java/site/iris/issuefy/entity/Issue.java
@@ -8,11 +8,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
-@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Table(name = "issue")
 public class Issue {
--- src/main/java/site/iris/issuefy/entity/Label.java
+++ src/main/java/site/iris/issuefy/entity/Label.java
@@ -6,11 +6,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
-@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Table(name = "label")
 public class Label {
