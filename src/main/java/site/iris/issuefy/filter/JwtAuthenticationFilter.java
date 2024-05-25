31831ecb4d82ca603eb1da63ce47574636e077eb
dokkisan <dokkisan.wang@gmail.com>
2024-05-25 18:52:43 +0900

    feat: 필터에 githubId 속성 추가
    - JWT 토큰에서 추출한 githubId를 request 속성으로 설정
    - 이후 필터 및 서블릿에서 githubId 속성 접근 가능하도록 개선

--- src/main/java/site/iris/issuefy/filter/JwtAuthenticationFilter.java
+++ src/main/java/site/iris/issuefy/filter/JwtAuthenticationFilter.java
@@ -10,6 +10,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import site.iris.issuefy.exception.UnauthenticatedException;
 import site.iris.issuefy.service.TokenProvider;
@@ -21,7 +22,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	private final TokenProvider tokenProvider;
 
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+	protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+									@NonNull FilterChain filterChain)
 		throws ServletException, IOException {
 
 		String path = request.getRequestURI();
@@ -37,6 +39,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 				throw new UnauthenticatedException();
 			}
 
+			request.setAttribute("githubId", tokenProvider.getClaims(token).getId());
 			filterChain.doFilter(request, response);
 		} catch (UnauthenticatedException e) {
 			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
