892f172ff2ad464d9210cb3a979a052432566ab1
dokkisan <dokkisan.wang@gmail.com>
2024-06-06 19:16:59 +0900

    chore: schema.sql 업데이트

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,28 +9,28 @@ INSERT INTO `user` (github_id, email)
 VALUES ('lvalentine6', 'user3@example.com');
 
 -- Inserting organizations
-INSERT INTO `org` (name)
-VALUES ('organization1');
-INSERT INTO `org` (name)
-VALUES ('organization2');
+INSERT INTO `org` (name, gh_org_id)
+VALUES ('organization1', 1);
+INSERT INTO `org` (name, gh_org_id)
+VALUES ('organization2', 2);
 
 -- Inserting repositories
-INSERT INTO `repository` (org_id, name)
-VALUES (1, 'repo-a1');
-INSERT INTO `repository` (org_id, name)
-VALUES (1, 'repo-a2');
-INSERT INTO `repository` (org_id, name)
-VALUES (2, 'repob-1');
+INSERT INTO `repository` (org_id, name, gh_repo_id)
+VALUES (1, 'repo-a1', 1);
+INSERT INTO `repository` (org_id, name, gh_repo_id)
+VALUES (1, 'repo-a2', 2);
+INSERT INTO `repository` (org_id, name, gh_repo_id)
+VALUES (2, 'repob-1', 3);
 
 -- Inserting issues
-INSERT INTO `issue` (repository_id, name)
-VALUES (1, 'issue-a1-1');
-INSERT INTO `issue` (repository_id, name)
-VALUES (1, 'issue-a1-2');
-INSERT INTO `issue` (repository_id, name)
-VALUES (2, 'issue-a2-1');
-INSERT INTO `issue` (repository_id, name)
-VALUES (3, 'issue-b1-1');
+INSERT INTO `issue` (repository_id, title, gh_issue_number)
+VALUES (1, 'issue-a1-1', 1234);
+INSERT INTO `issue` (repository_id, title, gh_issue_number)
+VALUES (1, 'issue-a1-2', 5678);
+INSERT INTO `issue` (repository_id, title, gh_issue_number)
+VALUES (2, 'issue-a2-1', 5679);
+INSERT INTO `issue` (repository_id, title, gh_issue_number)
+VALUES (3, 'issue-b1-1', 2000);
 
 -- Inserting labels
 INSERT INTO `label` (name)
