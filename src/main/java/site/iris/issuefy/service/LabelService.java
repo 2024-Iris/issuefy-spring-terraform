e72a51d9c8286ba1bc6d059f82c564a67577f1ce
dokkisan <dokkisan.wang@gmail.com>
2024-06-25 22:54:13 +0900

    fix: Collectors.toList() -> Stream.toList() 불변 리스트로 변경

--- src/main/java/site/iris/issuefy/service/LabelService.java
+++ src/main/java/site/iris/issuefy/service/LabelService.java
@@ -3,7 +3,6 @@ package site.iris.issuefy.service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -34,7 +33,7 @@ public class LabelService {
 		return optionalLabels
 			.map(labels -> labels.stream()
 				.map(LabelMapper.INSTANCE::labelEntityToLabelDto)
-				.collect(Collectors.toList()))
+				.toList())
 			.orElseGet(ArrayList::new);
 	}
 
