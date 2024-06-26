154b373edccd0ff631f6042ce1877270277be708
dokkisan <dokkisan.wang@gmail.com>
2024-06-26 23:11:05 +0900

    test: getLabelsByIssueId() 테스트 코드 작성

--- src/test/java/site/iris/issuefy/service/LabelServiceTest.java
+++ src/test/java/site/iris/issuefy/service/LabelServiceTest.java
@@ -14,6 +14,7 @@ import org.mockito.MockitoAnnotations;
 import site.iris.issuefy.entity.Label;
 import site.iris.issuefy.repository.LabelRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 class LabelServiceTest {
@@ -50,7 +51,7 @@ class LabelServiceTest {
 		assertEquals(labelColor, result.getColor());
 	}
 
-	@DisplayName("DB에 존재하는 레이블은 생성하지 않는다.")
+	@DisplayName("DB에 존재하는 레이블은 생성하지 않는다")
 	@Test
 	void findOrCreateLabel_Find() {
 		// given
@@ -68,5 +69,39 @@ class LabelServiceTest {
 		assertNotNull(result);
 		assertEquals(labelName, result.getName());
 		assertEquals(labelColor, result.getColor());
+	}
+
+	@DisplayName("이슈에 대한 레이블을 반환한다")
+	@Test
+	void getLabelsByIssueId_ReturnsLabels() {
+		List<Label> labels = List.of(
+			Label.of("test label name1", "000000"),
+			Label.of("test label name2", "111111")
+		);
+
+		// given
+		Long testIssueId = 1L;
+
+		// when
+		when(labelRepository.findByIssue_id(testIssueId)).thenReturn(Optional.of(labels));
+
+		// then
+		Optional<List<Label>> result = labelService.getLabelsByIssueId(testIssueId);
+		assertTrue(result.isPresent());
+		assertEquals(labels, result.get());
+	}
+
+	@DisplayName("이슈에 대한 레이블이 없다면 빈 리스트를 반환한다")
+	@Test
+	void getLabelsByIssueId_ReturnsEmpty() {
+		// given
+		Long testIssueId = 55L;
+
+		// when
+		when(labelRepository.findByIssue_id(testIssueId)).thenReturn(Optional.empty());
+
+		// then
+		Optional<List<Label>> result = labelService.getLabelsByIssueId(testIssueId);
+		assertFalse(result.isPresent());
 	}
 }
