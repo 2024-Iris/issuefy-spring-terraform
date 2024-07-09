4cb761af79d781be26b96640ba6c219ea6d6c87c
lvalentine6 <ysr1227@naver.com>
2024-07-09 21:23:49 +0900

    fix: 생성자 추가

--- src/main/java/site/iris/issuefy/component/LambdaKey.java
+++ src/main/java/site/iris/issuefy/component/LambdaKey.java
@@ -4,10 +4,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Component
 @Getter
+@NoArgsConstructor
 public class LambdaKey {
 	@Value("${jwt.lambdaKey}")
 	private String lambdaKey;
+
+	public LambdaKey(String lambdaKey) {
+		this.lambdaKey = lambdaKey;
+	}
 }
