b51345e287d26c0c792d571106e7224e8501b192
lvalentine6 <ysr1227@naver.com>
2024-07-10 20:58:08 +0900

    fix: 생성자 추가

--- src/main/java/site/iris/issuefy/model/dto/NotificationReadDto.java
+++ src/main/java/site/iris/issuefy/model/dto/NotificationReadDto.java
@@ -2,11 +2,15 @@ package site.iris.issuefy.model.dto;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NotificationReadDto {
 	List<Long> userNotificationIds;
 }
