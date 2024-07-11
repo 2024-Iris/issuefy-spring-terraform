c94ccf90bbc84b341fe49b997ccb7af52e6dc28e
lvalentine6 <ysr1227@naver.com>
2024-07-11 23:47:58 +0900

    fix: 생성자 추가

--- src/main/java/site/iris/issuefy/model/dto/GithubOrgDto.java
+++ src/main/java/site/iris/issuefy/model/dto/GithubOrgDto.java
@@ -1,10 +1,12 @@
 package site.iris.issuefy.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class GithubOrgDto {
 	private long id;
 	private String login;
