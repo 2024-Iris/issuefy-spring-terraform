ccfa233e3e4313bf22bc719e91fba3956daf61ba
lvalentine6 <ysr1227@naver.com>
2024-07-11 21:54:16 +0900

    feat: 생성자 추가

--- src/main/java/site/iris/issuefy/model/dto/UpdateRepositoryDto.java
+++ src/main/java/site/iris/issuefy/model/dto/UpdateRepositoryDto.java
@@ -2,10 +2,12 @@ package site.iris.issuefy.model.dto;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 public class UpdateRepositoryDto {
 	List<String> updatedRepositoryIds;
