77bcc4f44e5f357a6260ea2086536fcc5ab0c9a1
lvalentine6 <ysr1227@naver.com>
2024-07-22 01:51:30 +0900

    feat: 토큰 무효화 메서드 추가

--- src/main/java/site/iris/issuefy/service/TokenProvider.java
+++ src/main/java/site/iris/issuefy/service/TokenProvider.java
@@ -1,28 +1,44 @@
 package site.iris.issuefy.service;
 
 import java.security.Key;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
 import site.iris.issuefy.entity.Jwt;
+import site.iris.issuefy.model.dto.BlacklistedJwtDto;
 
 @Component
+@Slf4j
 public class TokenProvider {
+
+	private final RedisTemplate<String, BlacklistedJwtDto> redisTemplate;
 	private Key key;
 
-	public TokenProvider(@Value("${jwt.secretKey}") String secretKey) {
+	public TokenProvider(RedisTemplate<String, BlacklistedJwtDto> redisTemplate,
+		@Value("${jwt.secretKey}") String secretKey) {
 		this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+		this.redisTemplate = redisTemplate;
 	}
 
 	public Jwt createJwt(Map<String, Object> claims) {
@@ -51,15 +67,31 @@ public class TokenProvider {
 	}
 
 	public boolean isValidToken(String token) {
-		try {
-			Jws<Claims> claims = Jwts.parser()
-				.verifyWith((SecretKey)key)
-				.build()
-				.parseSignedClaims(token);
-			return claims.getPayload().getExpiration().after(new Date());
-		} catch (JwtException | IllegalArgumentException e) {
-			return false;
-		}
+		BlacklistedJwtDto blacklistedJwtDto = redisTemplate.opsForValue().get(token);
+        if (blacklistedJwtDto != null) {
+            return false;
+        }
+        try {
+            Jws<Claims> claims = Jwts.parser()
+                .verifyWith((SecretKey)key)
+                .build()
+                .parseSignedClaims(token);
+            return claims.getPayload().getExpiration().after(Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()));
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+	}
+
+	public void invalidateToken(String token) {
+		Claims claims = getClaims(token);
+        LocalDateTime expiresAt = claims.getExpiration().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        LocalDateTime now = LocalDateTime.now();
+
+        BlacklistedJwtDto blacklistedJwtDto = new BlacklistedJwtDto(token, now, expiresAt);
+
+        LocalDateTime redisExpirationTime = expiresAt.plusHours(1);
+        long redisExpirationSeconds = ChronoUnit.SECONDS.between(LocalDateTime.now(), redisExpirationTime);
+        redisTemplate.opsForValue().set(token, blacklistedJwtDto, redisExpirationSeconds, TimeUnit.SECONDS);
 	}
 
 	private Date getExpireDateAccessToken() {
