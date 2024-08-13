6e67786793bb8d2c8166ddb1d1a09676a44aec5d
lvalentine6 <ysr1227@naver.com>
2024-08-13 17:02:52 +0900

    fix: apply Optional

--- src/main/java/site/iris/issuefy/repository/SubscriptionRepository.java
+++ src/main/java/site/iris/issuefy/repository/SubscriptionRepository.java
@@ -12,19 +12,17 @@ import org.springframework.data.repository.query.Param;
 import site.iris.issuefy.entity.Subscription;
 
 public interface SubscriptionRepository extends CrudRepository<Subscription, Long> {
-	Page<Subscription> findByUserId(Long userId, Pageable pageable);
-
 	@Query("SELECT s FROM Subscription s JOIN s.repository r JOIN r.org " +
 		"WHERE s.user.id = :userId")
-	Page<Subscription> findPageByUserId(@Param("userId") Long userId, Pageable pageable);
+	Optional<Page<Subscription>> findPageByUserId(@Param("userId") Long userId, Pageable pageable);
 
 	@Query("SELECT s FROM Subscription s JOIN s.repository r JOIN r.org " +
 		"WHERE s.user.id = :userId AND s.repoStarred = true")
-	Page<Subscription> findPageByUserIdAndRepoStarredTrue(@Param("userId") Long userId, Pageable pageable);
+	Optional<Page<Subscription>> findPageByUserIdAndRepoStarredTrue(@Param("userId") Long userId, Pageable pageable);
 
 	Optional<Subscription> findByUserIdAndRepository_GhRepoId(Long userId, Long ghRepoId);
 
 	void deleteByRepository_GhRepoId(Long ghRepoId);
 
-	List<Subscription> findByRepositoryId(Long repositoryId);
+	Optional<List<Subscription>> findByRepositoryId(Long repositoryId);
 }
