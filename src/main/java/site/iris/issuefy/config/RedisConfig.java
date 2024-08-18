16197e129cef77f1cae28ed4a32790015b3cc3a9
lvalentine6 <ysr1227@naver.com>
2024-08-19 06:38:53 +0900

    fix: Redis 설정 변경

--- src/main/java/site/iris/issuefy/config/RedisConfig.java
+++ src/main/java/site/iris/issuefy/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package site.iris.issuefy.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -15,46 +16,60 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import site.iris.issuefy.service.NotificationService;
+import site.iris.issuefy.service.SseService;
 
 @Configuration
 public class RedisConfig {
 
-    @Bean
-    public RedisMessageListenerContainer redisMessageListenerContainer(LettuceConnectionFactory connectionFactory,
-            MessageListenerAdapter listenerAdapter) {
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.addMessageListener(listenerAdapter, notificationsTopic());
-        return container;
-    }
+	@Bean
+	public RedisMessageListenerContainer redisMessageListenerContainer(
+		LettuceConnectionFactory connectionFactory,
+		@Qualifier("notificationAdapter") MessageListenerAdapter notificationListenerAdapter,
+		@Qualifier("disconnectAdapter") MessageListenerAdapter disconnectListenerAdapter) {
+		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.addMessageListener(notificationListenerAdapter, notificationsTopic());
+		container.addMessageListener(disconnectListenerAdapter, disconnectTopic());
+		return container;
+	}
 
-    @Bean
-    public MessageListenerAdapter listenerAdapter(NotificationService notificationService) {
-        return new MessageListenerAdapter(notificationService, "handleRedisMessage");
-    }
+	@Bean(name = "notificationAdapter")
+	public MessageListenerAdapter listenerAdapter(NotificationService notificationService) {
+		return new MessageListenerAdapter(notificationService, "handleRedisMessage");
+	}
 
-    @Bean
-    public ChannelTopic notificationsTopic() {
-        return new ChannelTopic("notifications");
-    }
+	@Bean(name = "disconnectAdapter")
+	public MessageListenerAdapter disconnectListenerAdapter(SseService sseService) {
+		return new MessageListenerAdapter(sseService, "handleDisconnect");
+	}
 
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-        template.setKeySerializer(new StringRedisSerializer());
+	@Bean
+	public ChannelTopic notificationsTopic() {
+		return new ChannelTopic("notifications");
+	}
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+	@Bean
+	public ChannelTopic disconnectTopic() {
+		return new ChannelTopic("disconnect");
+	}
 
-        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+		template.setKeySerializer(new StringRedisSerializer());
 
-        template.setValueSerializer(serializer);
-        template.setHashValueSerializer(serializer);
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
-        template.afterPropertiesSet();
+		GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
 
-        return template;
-    }
+		template.setValueSerializer(serializer);
+		template.setHashValueSerializer(serializer);
+
+		template.afterPropertiesSet();
+
+		return template;
+	}
 }
