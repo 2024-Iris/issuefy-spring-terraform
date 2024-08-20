9a7aa87805412395731155479af8faf65dd4a4cb
lvalentine6 <ysr1227@naver.com>
2024-08-21 04:30:00 +0900

    fix: 중복, 타임아웃 커넥션 로직 수정

--- src/main/java/site/iris/issuefy/service/SseService.java
+++ src/main/java/site/iris/issuefy/service/SseService.java
@@ -7,8 +7,13 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import site.iris.issuefy.exception.code.ErrorCode;
+import site.iris.issuefy.exception.network.SseException;
+import site.iris.issuefy.model.SseStatus;
 import site.iris.issuefy.util.ContainerIdUtil;
 
 @Service
@@ -18,6 +23,7 @@ public class SseService {
 
 	private static final String CONTAINER_ID = ContainerIdUtil.getContainerId();
 	private static final String PREFIX_EMITTER = "emitter:";
+	private final ObjectMapper objectMapper;
 	private final RedisTemplate<String, String> redisTemplate;
 	private final ConcurrentHashMap<String, SseEmitter> emitters = new ConcurrentHashMap<>();
 
@@ -29,15 +35,16 @@ public class SseService {
 		}
 
 		if (isConnected(githubId)) {
-			removeEmitter(githubId);
+			completeConnection(githubId);
 		}
 
 		SseEmitter emitter = new SseEmitter(60000L);
 		addEmitter(githubId, emitter);
-		initConnect(emitter);
+		initConnect(githubId, emitter);
 
-		emitter.onTimeout(() -> removeEmitter(githubId));
-		emitter.onCompletion(() -> removeEmitter(githubId));
+		emitter.onTimeout(() -> completeConnection(githubId));
+
+		emitter.onCompletion(() -> completeConnection(githubId));
 
 		return emitter;
 	}
@@ -45,7 +52,7 @@ public class SseService {
 	public void handleDisconnect(String githubId) {
 		log.info("Received disconnect message for user: {}", githubId);
 		if (isConnected(githubId)) {
-			removeEmitter(githubId);
+			completeConnection(githubId);
 		}
 	}
 
@@ -54,22 +61,16 @@ public class SseService {
 		emitters.put(githubId, emitter);
 	}
 
-	public void removeEmitter(String githubId) {
-		redisTemplate.delete(PREFIX_EMITTER + githubId);
-		emitters.remove(githubId);
-	}
-
 	public void sendEventToUser(String githubId, String eventName, Object data) {
 		SseEmitter emitter = emitters.get(githubId);
 		if (emitter != null) {
 			try {
-				emitter.send(SseEmitter.event()
-					.id(String.valueOf(System.currentTimeMillis()))
-					.name(eventName)
-					.data(data));
+				emitter.send(
+					SseEmitter.event().id(String.valueOf(System.currentTimeMillis())).name(eventName).data(data));
 			} catch (IOException e) {
-				log.error("Failed to send event to user: {}", githubId, e);
-				removeEmitter(githubId);
+				completeConnection(githubId);
+				throw new SseException(ErrorCode.FAILED_SENDING_MESSAGE.getMessage(),
+					ErrorCode.FAILED_SENDING_MESSAGE.getStatus());
 			}
 		}
 	}
@@ -78,13 +79,23 @@ public class SseService {
 		return emitters.containsKey(githubId);
 	}
 
-	private void initConnect(SseEmitter sseEmitter) {
+	private void initConnect(String githubId, SseEmitter sseEmitter) {
 		try {
+			SseStatus status = SseStatus.INIT_CONNECTION;
+			String jsonData = objectMapper.writeValueAsString(status.getData());
 			sseEmitter.send(SseEmitter.event()
-				.name("initial")
-				.data("{\"message\": \"Connection established\"}"));
+				.name(status.getEventName())
+				.data(jsonData));
 		} catch (IOException e) {
-			log.error("fail init connection");
+			completeConnection(githubId);
+			throw new SseException(ErrorCode.FAILED_INIT_CONNECTION.getMessage(),
+				ErrorCode.FAILED_INIT_CONNECTION.getStatus());
 		}
+	}
+
+	public void completeConnection(String githubId) {
+		SseEmitter previousConnection = emitters.remove(githubId);
+		previousConnection.complete();
+		redisTemplate.delete(PREFIX_EMITTER + githubId);
 	}
 }
