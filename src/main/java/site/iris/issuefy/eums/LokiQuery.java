64ec3ab042f12df3ec62445c3d46adc9c258cb60
lvalentine6 <ysr1227@naver.com>
2024-09-05 07:53:26 +0900

    fix: query 일자 조정

--- src/main/java/site/iris/issuefy/eums/LokiQuery.java
+++ src/main/java/site/iris/issuefy/eums/LokiQuery.java
@@ -7,9 +7,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum LokiQuery {
 	NUMBER_OF_WEEKLY_VISIT(
-		"sum(count_over_time({job=\"issuefylog\"} |= \"[%s]\" |= \"Response: 200 GET /api/login - Method: login \" [7d]))"),
+		"sum(count_over_time({job=\"issuefylog\"} |= \"[%s]\" |= \"Response: 200 GET /api/login - Method: login \" [6d]))"),
 	NUMBER_OF_WEEKLY_REPOSITORY_ADDED(
-		"sum(count_over_time({job=\"issuefylog\"} |= \"[%s]\" |= \"Request: POST /api/subscriptions - Method: addRepository \" [7d]))");
+		"sum(count_over_time({job=\"issuefylog\"} |= \"[%s]\" |= \"Request: POST /api/subscriptions - Method: addRepository \" [6d]))");
 
 	private final String query;
 }
